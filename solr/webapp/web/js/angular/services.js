4d5118e30738b31a4243065266f7178870eaa6d0
Jan Høydahl <janhoy@users.noreply.github.com>
2023-09-05 10:57:04 +0200

    Return hash as arraubuffer, not HEX

--- solr/webapp/web/js/angular/services.js
+++ solr/webapp/web/js/angular/services.js
@@ -328,8 +328,8 @@ solrAdminServices.factory('System',
       }
 
       function sha256(str) {
-        var shaObj = new jsSHA("SHA-256", "TEXT", { encoding: "UTF8" });
-        return shaObj.update(str).getHash("HEX");
+        const shaObj = new jsSHA("SHA-256", "TEXT", { encoding: "UTF8" });
+        return shaObj.update(str).getHash("ARRAYBUFFER");
       }
 
       function base64urlencode(a) {
